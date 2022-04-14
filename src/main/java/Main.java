c212bc1dd5c7cc2854c1b811431870030a7d6e2b
Onggar <ext.oorynbasar@tinkoff.ru>
2022-04-14 22:32:46 +0600

    add http server

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,37 @@
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("");
+
+    public static void main(String[] args) throws Exception {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 8);
+        server.createContext("/test", new TestHandler());
+        server.start();
+    }
+
+    static class TestHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange t) throws IOException {
+            try (BufferedReader bodyReader = new BufferedReader(new InputStreamReader(t.getRequestBody()))) {
+                String bodyLine;
+
+                while ((bodyLine = bodyReader.readLine()) != null) {
+                    System.out.println(bodyLine);
+                }
+            }
+
+            String response = "This is the response";
+            t.sendResponseHeaders(200, response.length());
+            OutputStream os = t.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
     }
 }
